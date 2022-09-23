5f329182a613747c5e05ee570dbe9810ef4d6459
jiayuzhang128 <zjy_990315@163.com>
2022-09-23 23:26:53 +0800

    day6

---
+++ chapter1/programFlowStructure/nestedloop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    /* 嵌套循环 */
+    // 打印10行星号
+    for (int j = 0;j < 10;j++){
+        for (int i = 0;i < 10;i++){
+            cout << "* " ;
+        }
+        cout << endl;
+    }
+    cout << "=================================================================" << endl;
+    /* 九九乘法表 嵌套循环实现 */
+    // TODO
+}
